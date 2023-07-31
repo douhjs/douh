c0c9e73eca35b87600b946422b9410821732ab63
actav-n <gksdmltk02@gmail.com>
2023-07-31 23:21:41 +0900

    test: added middleware testcase for app

--- src/application/app.test.ts
+++ src/application/app.test.ts
@@ -1,3 +1,4 @@
+import * as request from 'supertest';
 import { Application } from './';
 describe('Douh application test', () => {
   it('should be defined', () => {
@@ -7,5 +8,33 @@ describe('Douh application test', () => {
   it('should instance can be created', () => {
     const app = new Application();
     expect(app).toBeDefined();
+  });
+
+  describe('middleware test', () => {
+    it('should middleware can return plain text', async () => {
+      const app = new Application();
+
+      app.use((req, res) => {
+        res.statusCode = 202;
+        return 'ok';
+      });
+
+      const response = await request(app.callback()).get('/');
+      expect(response.status).toBe(202);
+      expect(response.text).toBe('ok');
+    });
+
+    it('should middleware can return json', async () => {
+      const app = new Application();
+
+      app.use((req, res) => {
+        res.statusCode = 202;
+        return { message: 'ok' };
+      });
+
+      const response = await request(app.callback()).get('/');
+      expect(response.status).toBe(202);
+      expect(response.body).toStrictEqual({ message: 'ok' });
+    });
   });
 });
