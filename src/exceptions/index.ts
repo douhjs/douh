3f468b51771e44bb6e0a9030a9866aac42d0e189
Lee <actav@Leeui-MacBookPro.local>
2023-07-31 18:28:40 +0900

    chore: added some exceptions

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -19,6 +19,18 @@ export function badRequest(message?: string, data?: { errorMessage?: string }) {
   return new Exception(400, message ?? 'Bad Request', data);
 }
 
+export function unauthorized(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(401, message ?? 'Bad Request', data);
+}
+
+export function forbidden(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(403, message ?? 'Bad Request', data);
+}
+
 export function notFound(message?: string, data?: { errorMessage?: string }) {
   return new Exception(404, message ?? 'Not Found', data);
+}
+
+export function gone(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(410, message ?? 'Not Found', data);
 }
