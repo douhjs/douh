a795984bb874bf3e3572e76cf2b5c54d7896c536
actav-n <gksdmltk02@gmail.com>
2023-08-01 00:08:42 +0900

    test: added some failed testcases

--- src/router/router.test.ts
+++ src/router/router.test.ts
@@ -12,18 +12,68 @@ describe('router test', () => {
     expect(router).toBeDefined();
   });
 
-  it('should be worked as router', async () => {
+  it('should return plain text with return statement', async () => {
     const app = getApp();
     const router = new Router();
 
     router.get('/ping', (req, res) => {
-      res.statusCode = 203;
+      res.statusCode = 202;
       return 'pong';
     });
 
     app.use(router.middleware());
 
     const response = await request(app.callback()).get('/ping');
-    expect(response.statusCode).toBe(203);
+    expect(response.statusCode).toBe(202);
+    expect(response.text).toBe('pong');
+  });
+
+  it('should return json with return statement', async () => {
+    const app = getApp();
+    const router = new Router();
+
+    router.get('/ping', (req, res) => {
+      res.statusCode = 202;
+      return { message: 'pong' };
+    });
+
+    app.use(router.middleware());
+
+    const response = await request(app.callback()).get('/ping');
+    expect(response.statusCode).toBe(202);
+    expect(response.body).toStrictEqual({ message: 'pong' });
+  });
+
+  // FIXME: router이용해서 ctx.body = ''로 return하는 방식은 작동하지 않음.
+  it('should return plain text with "ctx.body = something" statement', async () => {
+    const app = getApp();
+    const router = new Router();
+
+    router.get('/ping', (req, res) => {
+      res.statusCode = 202;
+      res.body = 'pong';
+    });
+
+    app.use(router.middleware());
+
+    const response = await request(app.callback()).get('/ping');
+    expect(response.statusCode).toBe(202);
+    expect(response.text).toStrictEqual('pong');
+  });
+
+  it('should return json with "ctx.body = something" statement', async () => {
+    const app = getApp();
+    const router = new Router();
+
+    router.get('/ping', (req, res) => {
+      res.statusCode = 202;
+      res.body = { message: 'pong' };
+    });
+
+    app.use(router.middleware());
+
+    const response = await request(app.callback()).get('/ping');
+    expect(response.statusCode).toBe(202);
+    expect(response.body).toStrictEqual({ message: 'pong' });
   });
 });
