a12c517c386f672a0396c793449f048c8eae11ba
changhwan hwang <ch.hwang@ecubelabs.com>
2023-08-02 00:16:31 +0900

    feat: all 400 exceptions

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -59,6 +59,78 @@ export function gone(message?: string, data?: { errorMessage?: string }) {
   return new Exception(410, message ?? 'Gone', data);
 }
 
+export function lengthRequired(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(411, message ?? 'Length Required', data);
+}
+
+export function preconditionFailed(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(412, message ?? 'Precondition Failed', data);
+}
+
+export function payloadTooLarge(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(413, message ?? 'Payload Too Large', data);
+}
+
+export function uriTooLong(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(414, message ?? 'URI Too Long', data);
+}
+
+export function unsupportedMediaType(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(415, message ?? 'Unsupported Media Type', data);
+}
+
+export function rangeNotSatisfiable(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(416, message ?? 'Range Not Satisfiable', data);
+}
+
+export function expectationFailed(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(417, message ?? 'Expectation Failed', data);
+}
+
+export function imATeapot(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(418, message ?? "I'm a teapot", data);
+}
+
+export function misdirectedRequest(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(421, message ?? 'Misdirected Request', data);
+}
+
+export function unprocessableEntity(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(422, message ?? 'Unprocessable Entity', data);
+}
+
+export function locked(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(423, message ?? 'Locked', data);
+}
+
+export function failedDependency(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(424, message ?? 'Failed Dependency', data);
+}
+
+export function tooEarly(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(425, message ?? 'Too Early', data);
+}
+
+export function upgradeRequired(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(426, message ?? 'Upgrade Required', data);
+}
+
+export function preconditionRequired(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(428, message ?? 'Precondition Required', data);
+}
+
+export function tooManyRequests(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(429, message ?? 'Too Many Requests', data);
+}
+
+export function requestHeaderFieldsTooLarge(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(431, message ?? 'Request Header Fields Too Large', data);
+}
+
+export function unavailableForLegalReasons(message?: string, data?: { errorMessage?: string }) {
+  return new Exception(451, message ?? 'Unavailable For Legal Reasons', data);
+}
+
 export function notImplemented(message?: string, data?: { errorMessage?: string }) {
   return new Exception(501, message ?? 'Not Implemented', data);
 }
