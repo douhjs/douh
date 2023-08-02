48eeb12e2d2fa538aed68e36b0a8fdceaca85b11
ActaV <seungjun.lee@ecubelabs.com>
2023-08-02 18:27:19 +0900

    refactor: apply splitted response, request

--- src/body/parser.ts
+++ src/body/parser.ts
@@ -1,24 +1,9 @@
-import * as http from 'http';
+import { DouhRequest, DouhResponse } from '../http';
 import { NextFunction } from '../application';
-
-declare module 'http' {
-  interface IncomingMessage {
-    // NOTE: supply parse JSON body at Now
-    body?: {
-      [key: string]: any;
-    };
-    params: {
-      [key: string]: any;
-    };
-  }
-  interface ServerResponse {
-    body?: any;
-  }
-}
 
 const methodContainedBody = new Set(['POST', 'PUT', 'PATCH']);
 
-export function bodyParser(req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction) {
+export function bodyParser(req: DouhRequest, res: DouhResponse, next: NextFunction) {
   if (req.method && methodContainedBody.has(req.method)) {
     const buffers: Buffer[] = [];
     req.on('data', (chunk: Buffer) => {
