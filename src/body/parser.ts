5befb0e49b718e0e20e09f45dcd7ad81ddbcaf61
changhwan hwang <ch.hwang@ecubelabs.com>
2023-08-03 00:45:08 +0900

    refactor: body parse when request end event

--- src/body/parser.ts
+++ src/body/parser.ts
@@ -1,19 +1,39 @@
 import { DouhRequest, DouhResponse } from '../http';
 import { NextFunction } from '../application';
 
-const methodContainedBody = new Set(['POST', 'PUT', 'PATCH']);
+const methodsContainedBody = new Set(['POST', 'PUT', 'PATCH']);
 
-export function bodyParser(req: DouhRequest, res: DouhResponse, next: NextFunction) {
-  if (req.method && methodContainedBody.has(req.method)) {
-    const buffers: Buffer[] = [];
-    req.on('data', (chunk: Buffer) => {
-      buffers.push(chunk);
-      const payload = Buffer.concat(buffers).toString();
-      const body = JSON.parse(payload);
-      req.body = body ?? {};
+export async function bodyParser(req: DouhRequest, _: DouhResponse, next: NextFunction) {
+  if (req.method && methodsContainedBody.has(req.method)) {
+    const contentType = req.headers['content-type'];
+
+    if (contentType === 'application/json') {
+      // call next after parsing body
+      const body = await readStringifiedRequestBody(req);
+      req.body = body;
       next();
-    });
+    }
   } else {
     next();
   }
+}
+
+function readStringifiedRequestBody(req: DouhRequest) {
+  return new Promise((resolve, reject) => {
+    const buffers: Buffer[] = [];
+
+    req.on('data', (chunk: Buffer) => {
+      buffers.push(chunk);
+    });
+
+    req.on('end', () => {
+      const payload = Buffer.concat(buffers).toString();
+      const body = JSON.parse(payload);
+      resolve(body);
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+  }) as Promise<Record<string, any>>;
 }
